67f2ac64347aeee4569e97acbfbc7acaf5e0134a
Etienne Maheu <etienne.maheu@furrybuilder.com>
2015-05-20 22:24:45 -0400

    feature: Add support for SimpleInjector around the solution
    Add the SimpleInjector nuget package and configured it to support execution context scoping and auto-wire controllers.
    Add a centralized class for library initialization (Bootstrap) in all main projects.

---
+++ src/XprsIo.API/SimpleInjectorControllerActivator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using SimpleInjector;
+using System.Diagnostics;
+
+namespace XprsIo.API
+{
+    internal class SimpleInjectorControllerActivator : IControllerActivator
+    {
+        private Container _container;
+
+        public SimpleInjectorControllerActivator(Container _container)
+        {
+            this._container = _container;
+        }
+
+        [DebuggerStepThrough]
+        public object Create(ActionContext context, Type controllerType)
+        {
+            return _container.GetInstance(controllerType);
+        }
+    }
+}
--- src/XprsIo.API/Startup.cs
+++ src/XprsIo.API/Startup.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
+using Microsoft.AspNet.Mvc;
 using Microsoft.Framework.DependencyInjection;
+using SimpleInjector;
+using SimpleInjector.Diagnostics;
+using SimpleInjector.Extensions.ExecutionContextScoping;
 
 namespace XprsIo.API
 {
@@ -15,15 +14,50 @@ namespace XprsIo.API
         public Startup(IHostingEnvironment env)
         {
         }
+
+        private readonly Container _container = new Container();
         
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddInstance<IControllerActivator>(new SimpleInjectorControllerActivator(_container));
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeApplication(app);
+            RegisterControllers(app);
+
+            _container.Verify(VerificationOption.VerifyAndDiagnose);
+
+            app.Use(async (context, next) =>
+            {
+                using (_container.BeginExecutionContextScope())
+                {
+                    await next();
+                }
+            });
+            
             app.UseMvc();
+        }
+
+        private void InitializeApplication(IApplicationBuilder app)
+        {
+            Bootstrap.InitializeContainer(_container, app);
+            BusinessLayer.Bootstrap.InitializeContainer(_container);
+            DataAccessLayer.Bootstrap.InitializeContainer(_container);
+        }
+
+        private void RegisterControllers(IApplicationBuilder app)
+        {
+            var provider = app.ApplicationServices.GetRequiredService<IControllerTypeProvider>();
+            foreach (var type in provider.ControllerTypes)
+            {
+                var registration = Lifestyle.Transient.CreateRegistration(type, _container);
+                _container.AddRegistration(type, registration);
+                registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "ASP.NET disposes controllers.");
+            }
         }
     }
 }
