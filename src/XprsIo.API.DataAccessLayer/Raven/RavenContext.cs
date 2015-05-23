e937578e9ea91041558d834c820ee4d0b8610e36
Etienne Maheu <etienne.maheu@furrybuilder.com>
2015-05-23 15:37:20 -0400

    feature: Add a unit of work skeleton for RavenDB

---
+++ src/XprsIo.API.DataAccessLayer/Raven/RavenContext.cs
@@ -0,0 +1,39 @@
+﻿// //////////////////////////////////////////////////////////////////////////////////&#xD;
+// Licensed under the Apache License, Version 2.0 (the "License");&#xD;
+// you may not use this file except in compliance with the License.&#xD;
+// You may obtain a copy of the License at&#xD;
+// &#xD;
+//     http://www.apache.org/licenses/LICENSE-2.0&#xD;
+// &#xD;
+// Unless required by applicable law or agreed to in writing, software&#xD;
+// distributed under the License is distributed on an "AS IS" BASIS,&#xD;
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.&#xD;
+// See the License for the specific language governing permissions and&#xD;
+// limitations under the License.&#xD;
+// //////////////////////////////////////////////////////////////////////////////////
+
+using Raven.Client;
+using XprsIo.API.DataAccessLayer.Interfaces;
+
+namespace XprsIo.API.DataAccessLayer.Raven
+{
+    public class RavenContext : IRavenContext
+    {
+        private readonly IDocumentSession _session;
+
+        public RavenContext(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        public void SaveChanges()
+        {
+            _session.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _session.Dispose();
+        }
+    }
+}
