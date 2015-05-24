d28c5f2de002f584df90c8c8420037ced60c8814
Etienne Maheu <etienne.maheu@furrybuilder.com>
2015-05-23 21:41:57 -0400

    fix: Correct an issue with licenses containing html-escaped characters

--- src/XprsIo.API.DataAccessLayer/Interfaces/IUnitOfWork.cs
+++ src/XprsIo.API.DataAccessLayer/Interfaces/IUnitOfWork.cs
@@ -1,15 +1,15 @@
-﻿// //////////////////////////////////////////////////////////////////////////////////&#xD;
-// Licensed under the Apache License, Version 2.0 (the "License");&#xD;
-// you may not use this file except in compliance with the License.&#xD;
-// You may obtain a copy of the License at&#xD;
-// &#xD;
-//     http://www.apache.org/licenses/LICENSE-2.0&#xD;
-// &#xD;
-// Unless required by applicable law or agreed to in writing, software&#xD;
-// distributed under the License is distributed on an "AS IS" BASIS,&#xD;
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.&#xD;
-// See the License for the specific language governing permissions and&#xD;
-// limitations under the License.&#xD;
+﻿// //////////////////////////////////////////////////////////////////////////////////
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 // //////////////////////////////////////////////////////////////////////////////////
 
 namespace XprsIo.API.DataAccessLayer.Interfaces
