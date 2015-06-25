37dee7c7b3f684cb3e4e948baec59370deb55ea8
Etienne Maheu <contact@etiennemaheu.com>
2015-06-25 12:49:24 -0400

    feature: Add unit tests for DAL logic

---
+++ src/XprsIo.API.DataAccessLayer.Specs/Raven/Extensions/IdentityUserExtensionsSpecs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using Machine.Specifications;
+using XprsIo.API.DataAccessLayer.Entities.Identity;
+using XprsIo.API.DataAccessLayer.Providers.Raven.Extensions;
+
+namespace XprsIo.API.DataAccessLayer.Specs.Raven.Extensions
+{
+	[Subject(typeof (IdentityUserExtensions))]
+	public class When_GetUserName
+	{
+		Establish context = () => { _user = new IdentityUser { Id = "IdentityUsers/1" }; };
+
+		Because of = () => { _result = _user.GetUserName(); };
+
+		It should_be_the_username_part_of_the_id =
+			() => { _result.ShouldEqual("1"); };
+
+		static IdentityUser _user;
+		static string _result;
+	}
+
+	[Subject(typeof(IdentityUserExtensions))]
+	public class When_SetUserName
+	{
+		Establish context = () => { _user = new IdentityUser { Id = "IdentityUsers/1" }; };
+
+		Because of = () => { _user.SetUserName("2"); };
+
+		It should_set_the_id_to_the_username =
+			() => { _user.Id.ShouldEqual("IdentityUsers/2"); };
+
+		static IdentityUser _user;
+	}
+
+	[Subject(typeof(IdentityUserExtensions))]
+	public class When_Empty_ToIdentityUserId
+	{
+		Establish context = () => { _userName = string.Empty; };
+
+		Because of = () => { _exception = Catch.Exception(() => _userName.ToIdentityUserId()); };
+
+		It should_fail = 
+			() => { _exception.ShouldBeOfExactType<InvalidOperationException>(); };
+		It should_have_a_specific_reason =
+			() => { _exception.Message.ShouldContain("invalid"); };
+
+		static string _userName;
+		static Exception _exception;
+	}
+
+	[Subject(typeof(IdentityUserExtensions))]
+	public class When_Value_ToIdentityUserId
+	{
+		Establish context = () => { _userName = "1"; };
+
+		Because of = () => { _result = _userName.ToIdentityUserId(); };
+
+		It should_turn_the_username_as_a_valid_raven_id =
+			() => { _result.ShouldEqual("IdentityUsers/1"); };
+
+		static string _userName;
+		static string _result;
+	}
+
+	[Subject(typeof(IdentityUserExtensions))]
+	public class When_Empty_FromIdentityUserId
+	{
+		Establish context = () => { _id = string.Empty; };
+
+		Because of = () => { _exception = Catch.Exception(() => _id.FromIdentityUserId()); };
+
+		It should_fail =
+			() => { _exception.ShouldBeOfExactType<InvalidOperationException>(); };
+		It should_have_a_specific_reason =
+			() => { _exception.Message.ShouldContain("invalid"); };
+
+		static string _id;
+		static Exception _exception;
+	}
+
+	[Subject(typeof(IdentityUserExtensions))]
+	public class When_Value_FromIdentityUserId
+	{
+		Establish context = () => { _id = "IdentityUsers/1"; };
+
+		Because of = () => { _result = _id.FromIdentityUserId(); };
+
+		It should_be_the_username_part_of_the_id =
+			() => { _result.ShouldEqual("1"); };
+
+		static string _id;
+		static string _result;
+	}
+}
---
+++ src/XprsIo.API.DataAccessLayer.Specs/Raven/Queries/IdentityUserQueriesSpecs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Machine.Specifications;
+using XprsIo.API.DataAccessLayer.Entities.Identity;
+using XprsIo.API.DataAccessLayer.Interfaces;
+using XprsIo.API.DataAccessLayer.Providers.Raven.Extensions;
+using XprsIo.API.DataAccessLayer.Providers.Raven.Queries;
+using XprsIo.API.DataAccessLayer.Specs.Extensions;
+
+namespace XprsIo.API.DataAccessLayer.Specs.Raven.Queries
+{
+	[Subject(typeof (IdentityUserQueries))]
+	public class When_QueryByUserName
+	{
+		Establish context = () =>
+		{
+			_repository = Enumerable.Range(1, 3)
+				.Select(i => new IdentityUser { Id = i.ToString().ToIdentityUserId() })
+				.ToQueryableRepository();
+		};
+
+		Because of = () => { _result = _repository.QueryByUserName("2").ToArray(); };
+
+		It should_contain_a_single_value =
+			() => { _result.Length.ShouldEqual(1); };
+		It should_be_a_user_with_id_2 =
+			() => { _result[0].Id.ShouldEqual("2".ToIdentityUserId()); };
+
+		static IQueryableRepository<IdentityUser> _repository;
+		static IdentityUser[] _result;
+	}
+}
