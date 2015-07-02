5ba1c274a88edc8b0d124646b1b16e3491fec3b7
Etienne Maheu <etienne.maheu@furrybuilder.com>
2015-07-02 19:05:59 -0400

    feature: Add a RoleClaimService to properly support all IRoleStore optional interfaces

---
+++ src/XprsIo.API.IdentityProvider/Stores/Interfaces/IRoleClaimService.cs
@@ -0,0 +1,53 @@
+﻿// //////////////////////////////////////////////////////////////////////////////////
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// //////////////////////////////////////////////////////////////////////////////////
+
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using XprsIo.API.DataAccessLayer.Entities.Identity;
+
+namespace XprsIo.API.IdentityProvider.Stores.Interfaces
+{
+    /// <summary>
+    /// Stores role specific claims
+    /// 
+    /// </summary>
+    public interface IRoleClaimService
+    {
+        /// <summary>
+        /// Returns the claims for the role
+        /// 
+        /// </summary>
+        /// <param name="role"/><param name="cancellationToken"/>
+        /// <returns/>
+        Task<IList<Claim>> GetClaimsAsync(IdentityRole role, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Add a new role claim
+        /// 
+        /// </summary>
+        /// <param name="role"/><param name="claim"/><param name="cancellationToken"/>
+        /// <returns/>
+        Task AddClaimAsync(IdentityRole role, Claim claim, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Remove a role claim
+        /// 
+        /// </summary>
+        /// <param name="role"/><param name="claim"/><param name="cancellationToken"/>
+        /// <returns/>
+        Task RemoveClaimAsync(IdentityRole role, Claim claim, CancellationToken cancellationToken);
+    }
+}
---
+++ src/XprsIo.API.IdentityProvider/Stores/Raven/RoleClaimService.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using FluffIt.StaticExtensions;
+using XprsIo.API.DataAccessLayer.Entities.Identity;
+using XprsIo.API.DataAccessLayer.Providers.Raven.Interfaces;
+using XprsIo.API.IdentityProvider.Stores.Interfaces;
+
+namespace XprsIo.API.IdentityProvider.Stores.Raven
+{
+    public class RoleClaimService : IRoleClaimService
+    {
+        private readonly IAsyncRavenContext _context;
+
+        public RoleClaimService(IAsyncRavenContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IList<Claim>> GetClaimsAsync(IdentityRole role, CancellationToken cancellationToken)
+            => Task.FromResult((IList<Claim>)role.Claims.Select(c => new Claim(c.Key, c.Value)).ToList());
+
+        public Task AddClaimAsync(IdentityRole role, Claim claim, CancellationToken cancellationToken)
+        {
+            role.Claims.Add(new IdentityRoleClaim
+            {
+                Key = claim.Type,
+                Value = claim.Value
+            });
+
+            return TaskEx.Completed;
+        }
+
+        public Task RemoveClaimAsync(IdentityRole role, Claim claim, CancellationToken cancellationToken)
+        {
+            var storedClaims = role.Claims
+                .Where(c => c.Key == claim.Type);
+
+            foreach (var storedClaim in storedClaims)
+            {
+                role.Claims.Remove(storedClaim);
+            }
+
+            return TaskEx.Completed;
+        }
+    }
+}
